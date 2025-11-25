829e178718cf634014594d368c1c06554c9ea666
Christian Bonafena <christianbonafena7@gmail.com>
2025-11-25 15:54:48 +0700

    add accounting finance model

---
+++ addons/finance_accounting/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Finance & Accounting",
+    'summary': """
+        Finance and Accounting Module for DMNC""",
+    'description': """
+        Finance and Accounting Module with 3 main menus:
+        - Entry Data
+        - Pencarian & Laporan
+        - Pengaturan
+    """,
+    'author': "Arth",
+    'website': "http://www.yourcompany.com",
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/finance_accounting_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
